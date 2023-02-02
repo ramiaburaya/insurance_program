b279130a001f0e172d3486bdee59d41e1c019faf
Aburayya <ramiaburaya16@gmail.com>
2023-02-02 21:29:27 +0200

    Create Class person than the Client and Emp extend it

--- src/main/java/com/example/database_final_project/Employee.java
+++ src/main/java/com/example/database_final_project/Employee.java
@@ -1,33 +1,13 @@
 package com.example.database_final_project;
 
-public class Employee {
-    public Employee() {
-
-    }
-
+public class Employee extends Person {
     private String empID;
-    private int ssn;
-    private String firstName;
-    private String secondName;
-    private String thirdName;
-    private String fourthName;
-    private int phone_1;
-    private int phone_2;
-
-    private String date;
     private String password;
 
-    public Employee(String empID, int ssn, String firstName, String secondName, String thirdName, String fourthName, int phone_1,int phone_2, String date, String password) {
+    public Employee(String empID, int ssn, String firstName, String secondName, String thirdName, String fourthName, int phone_1, int phone_2, String date, String password) {
+        super(ssn, firstName, secondName, thirdName, fourthName
+                , phone_1, phone_2, date);
         this.empID = empID;
-        this.ssn = ssn;
-        this.firstName = firstName;
-        this.secondName = secondName;
-        this.thirdName = thirdName;
-        this.fourthName = fourthName;
-        this.phone_1 = phone_1;
-        this.phone_2 = phone_2;
-
-        this.date = date;
         this.password = password;
     }
 
@@ -39,63 +19,6 @@ public class Employee {
         this.empID = empID;
     }
 
-    public int getSsn() {
-        return ssn;
-    }
-
-    public void setSsn(int ssn) {
-        this.ssn = ssn;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getSecondName() {
-        return secondName;
-    }
-
-    public void setSecondName(String secondName) {
-        this.secondName = secondName;
-    }
-
-    public String getThirdName() {
-        return thirdName;
-    }
-
-    public void setThirdName(String thirdName) {
-        this.thirdName = thirdName;
-    }
-
-    public String getFourthName() {
-        return fourthName;
-    }
-
-    public void setFourthName(String fourthName) {
-        this.fourthName = fourthName;
-    }
-
-    public int getPhone_1() {
-        return phone_1;
-    }
-
-    public void setPhone_1(int phone_1) {
-        this.phone_1 = phone_1;
-    }
-
-
-    public String getDate() {
-        return date;
-    }
-
-    public void setDate(String date) {
-        this.date = date;
-    }
-
     public String getPassword() {
         return password;
     }
@@ -104,26 +27,12 @@ public class Employee {
         this.password = password;
     }
 
-    public int getPhone_2() {
-        return phone_2;
-    }
-
-    public void setPhone_2(int phone_2) {
-        this.phone_2 = phone_2;
-    }
 
     @Override
     public String toString() {
         return "Employee{" +
                 "empID='" + empID + '\'' +
-                ", ssn=" + ssn +
-                ", firstName='" + firstName + '\'' +
-                ", secondName='" + secondName + '\'' +
-                ", thirdName='" + thirdName + '\'' +
-                ", fourthName='" + fourthName + '\'' +
-                ", phone_1=" + phone_1 +
-                ", date='" + date + '\'' +
                 ", password='" + password + '\'' +
-                '}';
+                "} " + super.toString();
     }
 }
---
+++ src/main/java/com/example/database_final_project/Person.java
@@ -0,0 +1,102 @@
+package com.example.database_final_project;
+
+public abstract class Person {
+    private int ssn;
+    private String firstName;
+    private String secondName;
+    private String thirdName;
+    private String fourthName;
+    private int phone_1;
+    private int phone_2;
+
+    private String date;
+
+    public Person(int ssn, String firstName, String secondName, String thirdName, String fourthName, int phone_1, int phone_2, String date) {
+        this.ssn = ssn;
+        this.firstName = firstName;
+        this.secondName = secondName;
+        this.thirdName = thirdName;
+        this.fourthName = fourthName;
+        this.phone_1 = phone_1;
+        this.phone_2 = phone_2;
+        this.date = date;
+    }
+
+    public int getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(int ssn) {
+        this.ssn = ssn;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public String getThirdName() {
+        return thirdName;
+    }
+
+    public void setThirdName(String thirdName) {
+        this.thirdName = thirdName;
+    }
+
+    public String getFourthName() {
+        return fourthName;
+    }
+
+    public void setFourthName(String fourthName) {
+        this.fourthName = fourthName;
+    }
+
+    public int getPhone_1() {
+        return phone_1;
+    }
+
+    public void setPhone_1(int phone_1) {
+        this.phone_1 = phone_1;
+    }
+
+    public int getPhone_2() {
+        return phone_2;
+    }
+
+    public void setPhone_2(int phone_2) {
+        this.phone_2 = phone_2;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "ssn=" + ssn +
+                ", firstName='" + firstName + '\'' +
+                ", secondName='" + secondName + '\'' +
+                ", thirdName='" + thirdName + '\'' +
+                ", fourthName='" + fourthName + '\'' +
+                ", phone_1=" + phone_1 +
+                ", phone_2=" + phone_2 +
+                ", date='" + date + '\'' +
+                '}';
+    }
+}
